4dd3f2542a244de249985a02cc63e646557388ba
rio <75190035+HyoJongPark@users.noreply.github.com>
2025-03-12 19:07:36 +0900

    feat: implement patient data etl process

--- src/patient/patient.controller.ts
+++ src/patient/patient.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { PatientService } from './patient.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('patient')
-export class PatientController {}
+export class PatientController {
+  constructor(private readonly patientService: PatientService) {}
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: (req, file, callback) => {
+        if (!file.originalname.match(/\.(xlsx)$/)) {
+          return callback(
+            new BadRequestException(
+              '지원하지 않는 파일 형식입니다. (xlsx만 가능)',
+            ),
+            false,
+          );
+        }
+        callback(null, true);
+      },
+    }),
+  )
+  async getHello(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('데이터 파일이 존재하지 않습니다.');
+    }
+
+    const count: number = await this.patientService.upload(file.buffer);
+    return { success: true, count };
+  }
+}
--- src/patient/patient.service.ts
+++ src/patient/patient.service.ts
@@ -1,4 +1,127 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import * as xlsx from 'xlsx';
+import { Patient } from './entity/patient.entity';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
+
+const fieldMap: { [key: string]: keyof Patient } = {
+  차트번호: 'chart_number',
+  이름: 'name',
+  전화번호: 'phone',
+  주민등록번호: 'ssn',
+  주소: 'address',
+  메모: 'memo',
+};
 
 @Injectable()
-export class PatientService {}
+export class PatientService {
+  constructor(
+    @InjectEntityManager()
+    private readonly entityManager: EntityManager,
+  ) {}
+
+  async upload(buffer: Buffer) {
+    const workbook = xlsx.read(buffer, { type: 'buffer' });
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
+    const jsonData: Record<string, any>[] = xlsx.utils.sheet_to_json(sheet);
+
+    const patients: Patient[] = [];
+    for (const row of jsonData) {
+      const patient = new Patient();
+
+      Object.assign(
+        patient,
+        Object.fromEntries(
+          Object.entries(fieldMap)
+            .filter(([key]) => row[key] !== undefined)
+            .map(([key, value]) => [value, String(row[key]).trim()]),
+        ),
+      );
+
+      if (!this.isValidDataFormat(patient)) {
+        continue;
+      }
+
+      // translate
+      patient.phone = patient.phone.replace(/-/g, '');
+      patient.ssn = this.maskSSN(patient.ssn);
+
+      patients.push(patient);
+    }
+
+    await this.bulkInsertOrUpdate(patients);
+
+    return patients.length;
+  }
+
+  async bulkInsertOrUpdate(
+    patients: Patient[],
+    batchSize = 5000,
+  ): Promise<void> {
+    if (patients.length === 0) return;
+
+    const queryRunner = this.entityManager.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const batchPromises: Promise<any>[] = [];
+
+      for (let i = 0; i < patients.length; i += batchSize) {
+        const batch = patients.slice(i, i + batchSize);
+
+        const insertPromise = queryRunner.manager
+          .createQueryBuilder()
+          .insert()
+          .into(Patient)
+          .values(batch)
+          .orUpdate(
+            ['ssn', 'address', 'memo'],
+            ['name', 'phone', 'chart_number'],
+          )
+          .execute();
+
+        batchPromises.push(insertPromise);
+      }
+
+      await Promise.all(batchPromises);
+      await queryRunner.commitTransaction();
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException(
+        '내부 서버에서 문제가 발생했습니다.',
+      );
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  maskSSN(ssn: string): string {
+    if (/^\d{6}-\d{7}$/.test(ssn)) {
+      return `${ssn.substring(0, 8)}******`;
+    }
+    if (/^\d{6}$/.test(ssn)) {
+      return `${ssn}-0******`;
+    }
+    return ssn;
+  }
+
+  isValidDataFormat(patient: Patient) {
+    if (!patient.name || patient.name.length < 1 || patient.name.length > 16) {
+      return false;
+    }
+
+    if (!/^\d{11}$/.test(patient.phone)) {
+      return false;
+    }
+
+    if (
+      !/^\d{6}$/.test(patient.ssn) && // 990101
+      !/^\d{6}-\d{7}$/.test(patient.ssn) // 720226-3701592
+    ) {
+      return false;
+    }
+
+    return true;
+  }
+}
